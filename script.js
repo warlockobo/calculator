9ac27e0121659322be359df9f975d3288ea5040e
Wittner Andrews <wittnerandrews@gmail.com>
2025-03-30 23:43:05 -0500

    exponential added, working on overflow

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-const calcContainer = document.querySelector("#calcContainer");
 const numbers = document.querySelectorAll(".operand");
 const display = document.querySelector("#display");
 const clearButton = document.querySelector(".clear");
 const operators = document.querySelectorAll(".operator");
 const equalButton = document.querySelector(".equals");
 const decimal = document.querySelector("#decimal");
+const sign = document.querySelector(".sign");
+const percent = document.querySelector(".percent");
+
 let step = 1;
 let input1;
 let input2;
 let result;
 let operator2;
+let solved = 0;
+let exponential;
 
 
 for (let i=0; i<numbers.length; i++){
     let number = numbers[i];
     number.addEventListener('click' , () => {
-        if (display.innerHTML.length >= 12){
-            alert("You can't add more than 12 numbers. Sorry!");
+        if (solved == 0){
+
+                    if (display.innerHTML === "0") {
+                        display.innerHTML = number.innerHTML;
+                    }
+                    else {
+                        display.innerHTML += number.innerHTML;
+                    }
+
+                    if (step === 1){
+                    input1 = +display.innerHTML;
+
+                    }
+                    else {
+                    input2 = +display.innerHTML;
+                    }
+
         }
-        else {
-            if (display.innerHTML === "0") {
-                display.innerHTML = number.innerHTML;
-            }
-            else {
-                display.innerHTML += number.innerHTML;
-            }
-            if (step === 1){
-            input1 = +display.innerHTML;
-            }
-            else {
-            input2 = +display.innerHTML;
-            }
-    }
+        else  {
+            clear();
+        }
     }
 )};
 
@@ -47,7 +55,6 @@ decimal.addEventListener('click', () => {
 for (let i=0; i<operators.length; i++){
     let operator1 = operators[i];
     operator1.addEventListener('click', () => {
-        console.log(result);
         if (step == 2){
             calculate();
         }
@@ -55,21 +62,42 @@ for (let i=0; i<operators.length; i++){
             display.innerHTML = 0;
             step = 2;
             operator2 = operator1.innerHTML;
+            solved = 0;
         }
 
     }) 
 }
 
-clearButton.addEventListener("click", () => {
-    display.innerHTML = 0;
-    input1 = 0;
-    input2 = 0;
-    step = 1;
+sign.addEventListener('click', () => {
+    negative = +display.innerHTML;
+    negative = negative * -1;
+    display.innerHTML = negative;
+    if (step == 1){
+        input1 = negative;
+        }
+        else {
+        input2 = negative;
+        }
 });
 
+percent.addEventListener('click', () => {
+    percentage = +display.innerHTML;
+    percentage = percentage / 100;
+    display.innerHTML = percentage;
+    if (step == 1){
+    input1 = percentage;
+    }
+    else {
+    input2 = percentage;
+    }
+});
+
+clearButton.addEventListener("click", clear);
 equalButton.addEventListener("click", calculate);
 
 function calculate(){
+    input1 = Number(input1);
+    input2 = Number(input2);
     if (operator2 == "/" && input2 == ("0")){
         alert("Thought you were being clever huh?");
     }
@@ -89,9 +117,24 @@ function calculate(){
             break;
     }
     step = 1;
-    result = result.toFixed(2);
+    console.log(result);
+    result = Math.round(result * 100) / 100;
+    if (result.toString().length >= 12){
+        result = result.toExponential();
+        alert("My poor little calculator can't handle numbers this big :(");
+    }
+    console.log(result);
     display.innerHTML = result;
     input1 = result;
+    solved++;
 }
 }
 
+function clear(){
+    display.innerHTML = 0;
+    display.style.fontSize = '60px';
+    input1 = 0;
+    input2 = 0;
+    step = 1;
+    solved = 0;
+}
